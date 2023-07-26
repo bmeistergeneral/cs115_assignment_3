8d1084ec99373bd65781adacf07055fe75796c01
bmeistergeneral <brecon.morgan@gmail.com>
2023-07-26 11:58:36 -0400

    Completed part 2

--- ParseInts.java
+++ ParseInts.java
@@ -17,14 +17,16 @@ public class ParseInts {
         System.out.println("Enter a line of text");
         Scanner scanLine = new Scanner(scan.nextLine());
 
-        try {
-            while (scanLine.hasNext()) {
+
+        while (scanLine.hasNext()) {
+            try {
                 val = Integer.parseInt(scanLine.next());
                 sum += val;
-            }
-        } catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
 
+            }
         }
+
 
         System.out.println("The sum of the integers on this line is " + sum);
     }
